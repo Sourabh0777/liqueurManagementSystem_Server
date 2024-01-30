25d60db887a9ebfd274227f82986564f58135887
Sourabh <sourabh.verma9770@gmail.com>
2024-01-30 14:49:14 +0530

    Feature: core updated

--- src/user/controller/userAuthController.ts
+++ src/user/controller/userAuthController.ts
@@ -2,13 +2,12 @@ import { Request, Response } from "express";
 import * as userAuthService from "../services/auth.service";
 const userRegisterController = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
-  console.log("🚀 ~ name, email, password:", name, email, password);
   const userRegistrationResponse = await userAuthService.userRegisterService({
     name,
     email,
     password,
   });
-  return res.json(userRegistrationResponse);
+  return userRegistrationResponse.send(res);
 };
 
 export { userRegisterController };
--- src/user/methods/auth.method.ts
+++ src/user/methods/auth.method.ts
@@ -1,15 +1,19 @@
+import { Response } from "express";
 import prisma_client from "../../config/prisma";
+import { SuccessResponse } from "../../core/ApiResponse";
 import { userRegistrationInterface } from "../models/user.models";
 
 const RegisterUserMethod = async (userRegistrationData: userRegistrationInterface) => {
-  console.log("working");
+  console.log("api run till RegisterUserMethod");
   const registeredUser = await prisma_client.user.create({ data: { ...userRegistrationData } });
-  console.log("🚀 ~ RegisterUserMethod ~ registeredUser:", registeredUser);
-  return {
-    success: true,
-    data: registeredUser,
-    status: 200,
-    message: "registration successful",
-  };
+  return new SuccessResponse("Signup Successful", {
+    registeredUser: registeredUser,
+  });
+  // return {
+  //   success: true,
+  //   data: registeredUser,
+  //   status: 200,
+  //   message: "registration successful",
+  // };
 };
 export { RegisterUserMethod };
