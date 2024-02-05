73750660c5826ec28f094f0d03c1e9a627832ea1
Anupam Katoch <anukatoch90@gmail.com>
2024-02-05 14:33:03 +0530

    Committed

--- src/user/middlewares/generateUserJWT.ts
+++ src/user/middlewares/generateUserJWT.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET_KEY } from '../../secrets';
-const generateAuthToken = (phoneNumber: number, otp: number, id: number) => {
-  return jwt.sign({ phoneNumber }, JWT_SECRET_KEY, {
+const generateAuthToken = (phoneNumber: number, id: number) => {
+  return jwt.sign({ phoneNumber,id }, JWT_SECRET_KEY, {
     expiresIn: '1h',
   });
 };
