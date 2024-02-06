0d7bb97811aeb1a73be9b218734f2d90041449d9
Anupam Katoch <anukatoch90@gmail.com>
2024-02-06 22:59:14 +0530

    Committed

---
+++ src/vendor/models/vendor.models.ts
@@ -0,0 +1,24 @@
+export interface vendorRegistrationInterface {
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
+  username: string;
+  password: string;
+}
+export interface vendorDetails{
+    Id: number;
+    firstName: string;
+    lastName: string;
+    dateOfBirth: Date;
+    username: string;
+    phoneNumber: string;
+    addressLine1: string;
+    adressLine2: string;
+    city: string;
+    state: string;
+    country: string;
+    countryCode: string;
+    zipcode: string;
+    createdAt: Date;
+    upadatedAt: Date;
+}
--- src/vendor/vendor.routes.js
+++
@@ -1,4 +0,0 @@
-import express, { Request, Response } from 'express';
-const rootRouter = express();
-rootRouter.use('/vendor', vendorAuthController);
-export default rootRouter;
